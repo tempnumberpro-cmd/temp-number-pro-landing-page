4047aac412721de70e4a1954a8a3d15183262b6d
Leo00105 <183753142+Leo00105@users.noreply.github.com>
2026-05-05 10:18:21 -0400

    update

--- src/components/VirtualNumberDesc/VirtualNumberDesc.jsx
+++ src/components/VirtualNumberDesc/VirtualNumberDesc.jsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaFacebook, FaTwitter, FaGoogle } from 'react-icons/fa'
 import styles from './VirtualNumberDesc.module.css'
+import phoneFrame from '../../../assets/full-mobile-phone-frame.png'
 
 export default function VirtualNumberDesc() {
   return (
@@ -25,19 +26,24 @@ export default function VirtualNumberDesc() {
             <p>Get a virtual phone number in just a few minutes!</p>
           </div>
 
-          {/* Sign Up Card */}
-          <div className={styles.signUpCard}>
-            <h3 className={styles.cardTitle}>Sign Up</h3>
-            <input type="email" placeholder="Enter email" className={styles.input} />
-            <input type="password" placeholder="Create password" className={styles.input} />
-            <button className={styles.signUpBtn}>Sign Up</button>
-            <p className={styles.signIn}>Already have an account? <a href="#" className={styles.signInLink}>Sign in.</a></p>
-            <p className={styles.orText}>Or log in with your social network</p>
-            <div className={styles.socialRow}>
-              <button className={`${styles.socialBtn} ${styles.github}`}><FaGithub size={16} /></button>
-              <button className={`${styles.socialBtn} ${styles.facebook}`}><FaFacebook size={16} /></button>
-              <button className={`${styles.socialBtn} ${styles.twitter}`}><FaTwitter size={16} /></button>
-              <button className={`${styles.socialBtn} ${styles.google}`}><FaGoogle size={16} /></button>
+          {/* Sign Up phone preview */}
+          <div className={styles.phoneFrameWrap} aria-label="Sign up form shown inside mobile app frame">
+            <img src={phoneFrame} alt="" className={styles.phoneFrame} aria-hidden="true" />
+            <div className={styles.phoneScreen}>
+              <div className={styles.signUpCard}>
+                <h3 className={styles.cardTitle}>Sign Up</h3>
+                <input type="email" placeholder="Enter email" className={styles.input} />
+                <input type="password" placeholder="Create password" className={styles.input} />
+                <button className={styles.signUpBtn}>Sign Up</button>
+                <p className={styles.signIn}>Already have an account? <a href="#" className={styles.signInLink}>Sign in.</a></p>
+                <p className={styles.orText}>Or log in with your social network</p>
+                <div className={styles.socialRow}>
+                  <button className={`${styles.socialBtn} ${styles.github}`} aria-label="Continue with GitHub"><FaGithub size={16} /></button>
+                  <button className={`${styles.socialBtn} ${styles.facebook}`} aria-label="Continue with Facebook"><FaFacebook size={16} /></button>
+                  <button className={`${styles.socialBtn} ${styles.twitter}`} aria-label="Continue with Twitter"><FaTwitter size={16} /></button>
+                  <button className={`${styles.socialBtn} ${styles.google}`} aria-label="Continue with Google"><FaGoogle size={16} /></button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
